a3457796fd326d2e423bb147d44a9780145cf975
Spidjo <spidjo@users.noreply.github.com>
2025-05-24 13:11:13 +0200

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,1 +1,101 @@
+/*
+=================================================================================
+Create Silver Schema Tables
+=================================================================================
+Script purpose:
+    This script creates the silver layer tables in the Data Warehouse project.
+    The silver layer is used to store refined data that has been processed from the bronze layer.
+    The script checks if the tables already exist and drops them if they do before creating new ones.
 
+    WARNING: This script will drop existing tables if they exist.
+    Ensure you have backups or are aware of the data loss before running this script.
+    Change the database context to the Data Warehouse project
+*/
+-- Ensure you have the correct database context before running this script
+USE DataWarehousePrj;
+-- Create the table in silver schema if it doesn't exist
+IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_cust_info;  -- Check if the table exists
+-- Drop the table if it exists
+-- Create the table in silver schema
+CREATE TABLE silver.crm_cust_info (
+    cst_id INT, 
+    cst_key NVARCHAR(50),
+    cst_firstname NVARCHAR(50),
+    cst_lastname NVARCHAR(50),
+    cst_mirital_status NVARCHAR(50),
+    cst_gndr NVARCHAR(50),
+    cst_create_date DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE() 
+);
+
+-- Create the table in silver schema if it doesn't exist
+-- Check if the table exists
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_prd_info;  -- Drop the table if it exists
+-- Create the table in silver schema        
+CREATE TABLE silver.crm_prd_info(
+    prd_id INT,
+    cat_id NVARCHAR(50),
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost INT,
+    prd_line NVARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE() 
+);
+
+-- Create the table in silver schema if it doesn't exist
+-- Check if the table exists
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE silver.crm_sales_details; -- Drop the table if it exists
+-- Create the table in silver schema
+CREATE TABLE silver.crm_sales_details(
+    sls_ord_num NVARCHAR(50),
+    sls_prd_key NVARCHAR(50),
+    sls_cust_id INT,
+    sls_order_dt DATE,
+    sls_ship_dt DATE,
+    sls_due_dt DATE,
+    sls_sales INT,
+    sls_quantity INT,
+    sls_price INT,
+    dwh_create_date DATETIME2 DEFAULT GETDATE() 
+);
+
+-- Create the table in silver schema if it doesn't exist
+-- Check if the table exists
+IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE silver.erp_cust_az12; -- Drop the table if it exists
+-- Create the table in silver schema
+CREATE TABLE silver.erp_cust_az12 (
+    cid NVARCHAR(50),
+    BDATE DATE,
+    GEN NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE() 
+);
+
+-- Create the table in silver schema if it doesn't exist
+-- Check if the table exists
+IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE silver.erp_loc_a101; -- Drop the table if it exists
+-- Create the table in silver schema
+CREATE TABLE silver.erp_loc_a101 (
+    cid NVARCHAR(50),
+    cntry NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE() 
+);
+
+-- Create the table in silver schema if it doesn't exist
+-- Check if the table exists
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE silver.erp_px_cat_g1v2; -- Drop the table if it exists
+-- Create the table in silver schema
+CREATE TABLE silver.erp_px_cat_g1v2 (
+    id  NVARCHAR(50),
+    cat NVARCHAR(50),
+    subcat NVARCHAR(50),
+    maintenance NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE() 
+);
