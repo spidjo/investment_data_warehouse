aa71461f15988302dc741289d7c96c9ee11be3ea
Spidjo <spidjo@users.noreply.github.com>
2025-05-28 23:03:04 +0200

    Update sp_gold_load_aum_snapshot.sql

--- scripts/gold/sp_gold_load_aum_snapshot.sql
+++ scripts/gold/sp_gold_load_aum_snapshot.sql
@@ -1,13 +1,19 @@
-USE investment_dw;
-GO
-
 /*
-    -- Usage example:
+    Description: Procedure to insert a snapshot of the AUM data into the sp_gold_load_aum_snapshot table.
+                This Procedure takes 1 parameter (snapshot date)
+    Warning:    This procedure will delete/clear data for the date you are inserting.
+                Ensure that you have backups or that you are okay with losing existing data before running this procedure.
+    Example Usage: 
         BEGIN
         DECLARE @date DATE = GETDATE()
             EXEC gold.sp_load_aum_snapshot @snapshot_date = @date;
         END
 */
+
+USE investment_dw;
+GO
+
+
 -- Declare the snapshot date (you can also pass it as a parameter in a stored proc)
 CREATE OR ALTER PROCEDURE gold.sp_gold_load_aum_snapshot @snapshot_date DATE AS
 
@@ -33,7 +39,7 @@ latest_prices AS (
     FROM gold.v_security_prices
     WHERE market_date = @snapshot_date
 )
--- Insert into snapshot table (optional: comment this out if not inserting yet)
+-- Insert into snapshot table 
 INSERT INTO gold.tbl_aum_snapshot (
     snapshot_date,
     account_id,
